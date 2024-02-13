06640c9d5d0a82650d8ad1c22d60187b90ddc32c
Thanayut Seethongchuen <thanayut.see@outlook.com>
2024-02-14 00:01:28 +0100

    update ping-wg

--- ansible_collections/novicecpp/homeconfig/roles/wireguard/files/opt/ping-wg/ping-wg.sh
+++ ansible_collections/novicecpp/homeconfig/roles/wireguard/files/opt/ping-wg/ping-wg.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
+# Ping GATEWAY_IP every 30-60 seconds.
+# Retry 3 times (cooldown 10 seconds) before exit with error.
 
 if [[ -z $GATEWAY_IP ]]; then
    >&2 echo "WARNING: '\$GATEWAY_IP' does not set. Use default '172.29.0.1'."
@@ -7,6 +9,19 @@ fi
 GATEWAY_IP=${GATEWAY_IP:-172.29.0.1}
 
 while true; do
-    timeout 10s ping "${GATEWAY_IP}"
-    sleep $((240 + (RANDOM % 60) ))
+    for ((i=1;i<=3;i++)); do
+        ping -c5 "${GATEWAY_IP}" || rc=$?
+        if [[ -z $rc ]]; then
+            break;
+        fi
+        if [[ $i == 3 ]]; then
+            echo "Error: Maximum ping retry is reached."
+            exit 1;
+        fi
+        echo "Fail to ping ${GATEWAY_IP}. Retrying in 10 seconds (${i}/3)."
+        sleep 10
+    done
+    SLEEPSECONDS=$((30 + (RANDOM % 30)))
+    echo "Ping sucessfully. Sleeping $SLEEPSECONDS seconds."
+    sleep $SLEEPSECONDS
 done
