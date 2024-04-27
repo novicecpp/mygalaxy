25753315dda9ef42b2160e1c6dd04dc5191ea954
Thanayut Seethongchuen <thanyut.see@outlook.com>
2024-04-27 14:52:56 +0200

    also install galaxy

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
 set -euo pipefail
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-ln -s "${SCRIPT_DIR}"/novicecpp ~/.ansible/collections/ansible_collections/novicecpp
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+pushd "${SCRIPT_DIR}"
+ansible-galaxy install -r requirements.yml
+if [[ ! -L "${HOME}/.ansible/collections/ansible_collections/novicecpp" ]]; then
+    ln -s "${PWD}"/novicecpp "${HOME}/.ansible/collections/ansible_collections/novicecpp"
+fi
+popd
