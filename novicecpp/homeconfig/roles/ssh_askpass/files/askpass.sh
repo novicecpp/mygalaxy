938a13820eb061971c9fc446bd7fb287bdf2c63f
Thanayut Seethongchuen <thanyut.see@outlook.com>
2025-06-22 00:54:33 +0200

    control attempt at wrapper instead

--- novicecpp/homeconfig/roles/ssh_askpass/files/askpass.sh
+++ novicecpp/homeconfig/roles/ssh_askpass/files/askpass.sh
@@ -2,31 +2,21 @@
 
 set -euo pipefail
 
-ATTEMPT_COUNT_FILE="/tmp/askpass_count_u$(id -u).txt"
-# use PASSPATH_SSH_KEY define in home-config/private
-PASSPATH_SSH_KEY=${PASSPATH_SSH_KEY:-probe}
-MAX_ATTEMPTS=3
+# require env (see f_ssh_add()),
+[[ -v PASS_KEY_PATH ]] || echo "PASS_KEY_PATH is not set." >&2
+[[ -v ATTEMPT_FILE ]] || echo "ATTEMPT_FILE is not set." >&2
 
-if [ -f "$ATTEMPT_COUNT_FILE" ]; then
-    CURRENT_ATTEMPTS=$(cat "${ATTEMPT_COUNT_FILE}")
-else
-    CURRENT_ATTEMPTS=0
-fi
-
-CURRENT_ATTEMPTS=$(( CURRENT_ATTEMPTS + 1 ))
-echo "${CURRENT_ATTEMPTS}" > "${ATTEMPT_COUNT_FILE}"
-
-if [[ "$CURRENT_ATTEMPTS" -gt "$MAX_ATTEMPTS" ]]; then
-    rm -f "$ATTEMPT_COUNT_FILE"
-    echo "Error: Maximum password attempts exceeded. Exiting..." >&2
+if [[ -f "$ATTEMPT_FILE" ]]; then
+    rm -f "$ATTEMPT_FILE"
+    echo "Error: The password already provided. Please check passphrase from \"${PASS_KEY_PATH}\". Exiting..." >&2
     exit 1
 else
-    PASSPHRASE="$(pass show ${PASSPATH_SSH_KEY})"
+    PASSPHRASE="$(pass show ${PASS_KEY_PATH})"
     if [[ -z "${PASSPHRASE}" ]]; then
         echo "Error: password is empty." >&2
-        rm -f "${ATTEMPT_COUNT_FILE}"
         exit 1
     else
+        touch "${ATTEMPT_FILE}"
         echo "${PASSPHRASE}"
     fi
 fi
